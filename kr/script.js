ac9aa9926a933fb194865309e30db306f2a1936c
PCS0206 <127528087+PCS0206@users.noreply.github.com>
2024-06-01 22:16:05 +0800

    Update script.js

--- kr/script.js
+++ kr/script.js
@@ -3,9 +3,9 @@ document.addEventListener('DOMContentLoaded', (event) => {
   languageSelect.addEventListener('change', (event) => {
       const selectedLanguage = event.target.value;
       if (selectedLanguage === 'en') {
-          window.location.href = 'https://example.com/en';
+          window.location.href = 'https://pcs0206.github.io/parkchaesung/en/';
       } else if (selectedLanguage === 'ko') {
-          window.location.href = 'https://example.com/ko';
+          window.location.href = 'https://pcs0206.github.io/parkchaesung/kr/';
       }
   });
 
