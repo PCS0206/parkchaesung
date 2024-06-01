84ee54ee397e888cdea08f58e799584ee01f1fd2
PCS0206 <127528087+PCS0206@users.noreply.github.com>
2024-06-01 22:24:17 +0800

    Update script.js

--- kr/script.js
+++ kr/script.js
@@ -2,17 +2,9 @@ document.addEventListener('DOMContentLoaded', (event) => {
   const languageSelect = document.getElementById('language-select');
   languageSelect.addEventListener('change', (event) => {
       const selectedLanguage = event.target.value;
-      if (selectedLanguage === 'en') {
-          window.location.href = 'https://pcs0206.github.io/parkchaesung/en/';
-      } else if (selectedLanguage === 'ko') {
+      if (selectedLanguage === 'ko') {
           window.location.href = 'https://pcs0206.github.io/parkchaesung/kr/';
+      } else if (selectedLanguage === 'en') {
+          window.location.href = 'https://pcs0206.github.io/parkchaesung/en/';
       }
   });
-
-  const buttons = document.querySelectorAll('.buttons button');
-  buttons.forEach(button => {
-      button.addEventListener('click', () => {
-          alert(button.textContent + ' clicked');
-      });
-  });
-});
