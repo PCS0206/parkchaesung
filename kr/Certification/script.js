6d419737070a8402667fe1d2e2580320e118772b
PCS0206 <127528087+PCS0206@users.noreply.github.com>
2024-06-23 12:14:22 +0800

    Update script.js

--- kr/Certification/script.js
+++ kr/Certification/script.js
@@ -1,3 +1,11 @@
-function goBack() {
-    window.history.back();
-}
+document.addEventListener('DOMContentLoaded', (event) => {
+  const languageSelect = document.getElementById('language-select');
+  languageSelect.addEventListener('change', (event) => {
+      const selectedLanguage = event.target.value;
+      if (selectedLanguage === 'ko') {
+          window.location.href = 'https://pcs0206.github.io/parkchaesung/kr/';
+      } else if (selectedLanguage === 'en') {
+          window.location.href = 'https://pcs0206.github.io/parkchaesung/en/';
+      }
+  });
+});
